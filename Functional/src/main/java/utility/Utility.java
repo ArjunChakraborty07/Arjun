5007e7b029bacb5e6c2d984cf7fb8e856ca5e33c
ArjunChakraborty07 <chakraborty.arjun6@gmail.com>
2020-01-10 13:59:39 +0530

    [Arjun Chakraborty]Binary search using generic done

--- Functional/src/main/java/utility/Utility.java
+++ Functional/src/main/java/utility/Utility.java
@@ -104,7 +104,7 @@ public class Utility {
 			while(beg<=end)
 			   {
 			     int mid=(beg+end)/2;
-			     if (arr[mid].compareToIgnoreCase(str) < 0)
+			     if (arr[mid].compareToIgnoreCase(str) < 0)			     
 			     {
 			         beg=mid+1;
 			     }
@@ -120,11 +120,11 @@ public class Utility {
 			System.out.println("Word not found!!");
 			return -1;
 		}
-		public static String[] sort(String s[]) {
-			String tmp;
+		public static <T extends Comparable<? super T>>T[] sort(T s[]) {
+			T tmp;
 			for (int i = 0; i < s.length; i++) {
 				for (int j = 0; j < s.length; j++) {
-					if (s[i].compareToIgnoreCase(s[j]) < 1) {
+					if (s[i].compareTo(s[j]) < 1) {
 						tmp = s[i];
 						s[i] = s[j];
 						s[j] = tmp;
@@ -263,7 +263,7 @@ public class Utility {
 	            }
 	            return arr;
 	      }
-	      public static< T extends Comparable<? super T>>void selectionSort(T[] arr)
+	      public static< T extends Comparable<? super T>>void selectionSortgeneric(T[] arr)
 	      {
 	    	  for(int i=0;i<arr.length-1;i++)
 	    	  {	  int min=i;
@@ -279,6 +279,27 @@ public class Utility {
 	    	  arr[min]=temp;
 	    	  }
 	      }
+	      public static<T extends Comparable<? super T>> int binarySearchgeneric(T arr[], int beg, int end, T str)
+			{		
+				while(beg<=end)
+				   {
+				     int mid=(beg+end)/2;
+				     if (arr[mid].compareTo(str) < 0)			     
+				     {
+				         beg=mid+1;
+				     }
+				     else if (arr[mid].compareTo(str) > 0)
+				     {
+				         end=mid-1;
+				     }
+				     else
+				     {
+				         return mid;
+				     }
+				   }
+				System.out.println("Word not found!!");
+				return -1;
+			}
 }
 	    
 
