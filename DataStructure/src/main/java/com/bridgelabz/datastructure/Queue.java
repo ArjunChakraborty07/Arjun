bf8bf6e5df3baef35f687dc93fb9274db5aa62b3
ArjunChakraborty07 <chakraborty.arjun6@gmail.com>
2020-01-16 17:38:41 +0530

    [Arjun Chakraborty] Queue banking simulation added

---
+++ DataStructure/src/main/java/com/bridgelabz/datastructure/Queue.java
@@ -0,0 +1,84 @@
+package com.bridgelabz.datastructure;
+
+public class Queue {
+	static class Node{
+		int data;
+		Node next;
+		Node(int d)
+		{
+			data=d;
+			next=null;
+		}
+	}
+	static Node head;
+	public static void enqueue(int data)
+	{
+		Node now=new Node(data);
+		if(head==null)
+		{
+			head=now;
+		}
+		else
+		{
+			Node last=head;
+			while(last.next!=null)
+			{
+				last=last.next;
+			}
+			last.next=now;
+		}
+		
+	}
+	public static void dequeue()
+	{
+		head=head.next;		
+	}
+	public static void printlist()
+	{
+		Node print=head;
+		System.out.println("Queue");
+		while(print!=null)
+		{
+			System.out.print(print.data+" ");
+			print=print.next;
+		}	
+		System.out.println();
+	}
+	
+	public static void peek()
+	{
+		System.out.println(head.data);
+	}
+	public static boolean isempty()
+	{
+		if(head==null)
+		{
+			return true;
+		}
+		return false;
+	}
+	public static int size()
+	{
+		int count=0;
+		Node ctr=head;
+		while(ctr!=null)
+		{
+			count++;
+			ctr=ctr.next;
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		enqueue(5);
+		enqueue(4);
+		enqueue(1);
+		enqueue(8);
+		enqueue(7);
+		printlist();
+		dequeue();
+		printlist();
+		peek();
+		System.out.println(isempty());
+		System.out.println(size());
+	}
+}
