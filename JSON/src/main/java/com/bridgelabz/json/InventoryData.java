850bc010647dbf3a3277195b5979cbc7e69e7a3a
ArjunChakraborty07 <chakraborty.arjun6@gmail.com>
2020-01-22 17:52:22 +0530

    [Arjun Chakraborty] Inventory management updated and done

--- JSON/src/main/java/com/bridgelabz/json/InventoryData.java
+++ JSON/src/main/java/com/bridgelabz/json/InventoryData.java
@@ -55,6 +55,33 @@ public class InventoryData {
 		}
 	}
 		
+	public static void edit(String name, JSONArray inventory) throws JSONException
+	{
+		JSONObject obj=null;
+		for(int i=0;i<inventory.length();i++)
+		{
+			obj=(JSONObject) inventory.get(i);
+			if(obj.get("Name").equals(name))
+			{
+				int ch;
+				System.out.print(" Enter the change for"+name+" (1.weight/2.Price) ");
+				ch=utility.Utility.returnInt();
+				System.out.print(" Enter the updated value: ");
+				if(ch==1)
+				{	
+					int value=utility.Utility.returnInt();
+					obj.remove("Weight");
+					obj.put("Weight", value);
+				}
+				else
+				{
+					int value=utility.Utility.returnInt();
+					obj.put("Price", value);
+				}
+				
+			}
+		}
+	}
 	public static void display(JSONObject obj, JSONArray inventory) throws JSONException
 	{
 		int grandtotal=0;
@@ -85,8 +112,9 @@ public class InventoryData {
 		//add( inventory);			
 		//writeFile(inventory);  
 		//display(obj,inventory);	
-		delete("s",inventory);
-		delete("q",inventory);
+		//delete("s",inventory);
+		//delete("q",inventory);
+		edit("Rice", inventory);
 		writeFile(inventory); 
 		display(obj,inventory);	
 	}
