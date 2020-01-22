eab204f4f35c21cbd685b1550a4071dfd706f8a5
ArjunChakraborty07 <chakraborty.arjun6@gmail.com>
2020-01-22 17:14:42 +0530

    [Arjun Chakraborty]Inventory Management Extension Done

--- JSON/src/main/java/com/bridgelabz/json/InventoryData.java
+++ JSON/src/main/java/com/bridgelabz/json/InventoryData.java
@@ -15,18 +15,48 @@ import org.json.simple.parser.ParseException;
 
 public class InventoryData {
 	
-	
-	public static void main(String[] args) throws IOException, ParseException, JSONException {
-		
-		JSONObject obj=null;
+	public static JSONArray readFile() throws JSONException, IOException, ParseException
+	{
 		JSONParser jsonParser = new JSONParser();
-		JSONArray inventory = null;
-		try {
+		JSONArray inventory=new JSONArray();
 		FileReader reader = new FileReader("/home/admin1/Desktop/Inventory.json");
-		// Read JSON file
-		
 		inventory = new JSONArray(jsonParser.parse(reader).toString());
-		//Array object storing the text as an array
+		return inventory;
+	}
+	
+	public static void add(JSONArray inventory) throws JSONException	
+	{
+		JSONObject obj=new JSONObject();
+		int t=inventory.length();	
+		int weight, price;
+		String name;
+		System.out.println(" Enter a name:");
+		name=utility.Utility.returnString();
+		obj.put("Name", name);
+		System.out.println(" Enter a weight:");
+		weight=utility.Utility.returnInt();
+		obj.put("Weight", weight);
+		System.out.println(" Enter a price:");
+		price=utility.Utility.returnInt();					    
+	    obj.put("Price", price);
+		inventory.put(obj);	    	 	    
+	}
+	
+	public static void delete(String name, JSONArray inventory) throws JSONException
+	{
+		JSONObject obj=null;
+		for(int i=0;i<inventory.length();i++)
+		{
+			obj=(JSONObject) inventory.get(i);
+			if(obj.get("Name").equals(name))
+			{
+				inventory.remove(i);
+			}
+		}
+	}
+		
+	public static void display(JSONObject obj, JSONArray inventory) throws JSONException
+	{
 		int grandtotal=0;
 		for(int i=0;i< inventory.length();i++)// Processing the array
 		{
@@ -39,16 +69,26 @@ public class InventoryData {
 			grandtotal+=Total;
 		}
 		System.out.println("Grand Total Price: " +grandtotal);
+	}
 		
-		} catch (FileNotFoundException e) {
-		e.printStackTrace();
-		} catch (IOException e) {
-		e.printStackTrace();
-		} catch (ParseException e) {
-		e.printStackTrace();
-		}
-  
-        
+	public static void writeFile(JSONArray inventory) throws IOException
+	{
+		FileWriter file = new FileWriter("/home/admin1/Desktop/Inventory.json");
+        file.append((inventory).toString());
+        file.close();        
+	}	
+	
+	public static void main(String[] args) throws JSONException, IOException, ParseException {
+		
+		JSONObject obj=null;									
+		JSONArray inventory = readFile();					
+		//add( inventory);			
+		//writeFile(inventory);  
+		//display(obj,inventory);	
+		delete("s",inventory);
+		delete("q",inventory);
+		writeFile(inventory); 
+		display(obj,inventory);	
 	}
 
 }
